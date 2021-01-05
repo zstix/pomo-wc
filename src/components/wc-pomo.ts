5f4a401da9870c7452c009c061dc76a5b40845db
Zack Stickles <zstickles@gmail.com>
2021-01-05 08:53:33 -0800

    chore: web component

---
+++ src/components/wc-pomo.ts
@@ -0,0 +1,8 @@
+import { LitElement, html, customElement } from 'lit-element';
+
+@customElement('wc-pomo')
+export class WCPomo extends LitElement {
+  render() {
+    return html`<div>Hello from LitElement</div>`;
+  }
+}
